c18e5206b87b47085f5943b786eb7e9b31742252
Badgujar Dipesh <117906055+dipeshb1999@users.noreply.github.com>
2023-06-16 17:56:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,45 @@
-//complete this code
-class Rectangle {}
+class Rectangle {
+  constructor(width, height) {
+    this._width = width;
+    this._height = height;
+  }
 
-class Square extends Animal {}
+  get width() {
+    return this._width;
+  }
 
-// Do not change the code below this line
-window.Rectangle = Rectangle;
-window.Square = Square;
+  get height() {
+    return this._height;
+  }
+
+  getArea() {
+    return this._width * this._height;
+  }
+}
+
+class Square extends Rectangle {
+  constructor(sideLength) {
+    super(sideLength, sideLength);
+  }
+
+  getPerimeter() {
+    return 4 * this._width;
+  }
+}
+
+// Create instances and display output
+const rectangle = new Rectangle(5, 10);
+const square = new Square(4);
+
+const outputDiv = document.getElementById('output');
+outputDiv.innerHTML = `
+  <p>Rectangle:</p>
+  <p>Width: ${rectangle.width}</p>
+  <p>Height: ${rectangle.height}</p>
+  <p>Area: ${rectangle.getArea()}</p>
+  <br>
+  <p>Square:</p>
+  <p>Side Length: ${square.width}</p>
+  <p>Area: ${square.getArea()}</p>
+  <p>Perimeter: ${square.getPerimeter()}</p>
+`;
